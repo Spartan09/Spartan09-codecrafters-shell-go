3bcc290b88aa9408409b4dd24e933ba5d9e2e252
Ayushman <ayushsg0412@gmail.com>
2024-12-22 00:43:53 +0530

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -8,68 +8,109 @@ import (
 	"strings"
 )
 
-type CommandHandler func(args []string)
-
-var commands map[string]CommandHandler
-
-func exitHandler(args []string) {
-	os.Exit(0)
+type Command interface {
+	Execute(args []string) error
+	Name() string
 }
 
-func echoHandler(args []string) {
-	echo := strings.Join(args, " ")
-	fmt.Printf("%s\n", echo)
+type Shell struct {
+	commands map[string]Command
 }
 
-func typeHandler(args []string) {
-	if len(args) != 1 {
-		return
+func NewShell() *Shell {
+	s := &Shell{
+		commands: make(map[string]Command),
 	}
-	_, exists := commands[args[0]]
-	if exists {
-		fmt.Printf("%s is a shell builtin\n", args[0])
-		return
-	}
-	envPath := os.Getenv("PATH")
-	paths := strings.Split(envPath, ":")
-	//fmt.Printf("CMD: %s\n", cmd)
-	//fmt.Printf("PATH: %s\n", envPath)
-	for _, path := range paths {
-		fp := filepath.Join(path, args[0])
-		if _, err := os.Stat(fp); err == nil {
-			fmt.Println(fp)
-			return
-		}
-	}
-	fmt.Printf("%s: not found\n", args[0])
+	s.registerBuiltins()
+	return s
 }
 
-func main() {
-	commands = map[string]CommandHandler{
-		"exit": exitHandler,
-		"echo": echoHandler,
-		"type": typeHandler,
-	}
+func (s *Shell) registerBuiltins() {
+	s.commands["exit"] = &ExitCommand{}
+	s.commands["echo"] = &EchoCommand{}
+	s.commands["type"] = &TypeCommand{shell: s}
+}
+
+func (s *Shell) Run() error {
+	reader := bufio.NewReader(os.Stdin)
+
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 
-		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		input, err := reader.ReadString('\n')
 		if err != nil {
+			return fmt.Errorf("reading input: %w", err)
+		}
+
+		if err := s.Execute(input); err != nil {
 			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
 		}
+	}
+}
 
-		command = strings.TrimSuffix(command, "\n")
-		parts := strings.Fields(command)
+func (s *Shell) Execute(input string) error {
+	parts := strings.Fields(strings.TrimSpace(input))
+	if len(parts) == 0 {
+		return nil
+	}
 
-		if len(parts) == 0 {
-			continue
+	cmd, exists := s.commands[parts[0]]
+	if !exists {
+		return fmt.Errorf("%s: command not found", parts[0])
+	}
+
+	return cmd.Execute(parts[1:])
+}
+
+type ExitCommand struct{}
+
+func (c *ExitCommand) Name() string { return "exit" }
+func (c *ExitCommand) Execute(args []string) error {
+	os.Exit(0)
+	return nil
+}
+
+type EchoCommand struct{}
+
+func (c *EchoCommand) Name() string { return "echo" }
+func (c *EchoCommand) Execute(args []string) error {
+	fmt.Println(strings.Join(args, " "))
+	return nil
+}
+
+type TypeCommand struct {
+	shell *Shell
+}
+
+func (c *TypeCommand) Name() string { return "type" }
+func (c *TypeCommand) Execute(args []string) error {
+	if len(args) != 1 {
+		return fmt.Errorf("type: incorrect number of arguments")
+	}
+	if _, exists := c.shell.commands[args[0]]; exists {
+		fmt.Printf("%s is a shell builtin\n", args[0])
+		return nil
+	}
+	return c.searchInPath(args[0])
+}
+
+func (c *TypeCommand) searchInPath(cmd string) error {
+	envPath := os.Getenv("PATH")
+	paths := strings.Split(envPath, ":")
+	for _, path := range paths {
+		fp := filepath.Join(path, cmd)
+		if _, err := os.Stat(fp); err == nil {
+			fmt.Println(fp)
+			return nil
 		}
+	}
+	return fmt.Errorf("%s: not found", cmd)
+}
 
-		if handler, exists := commands[parts[0]]; exists {
-			handler(parts[1:]) // Pass remaining args to handler
-		} else {
-			fmt.Printf("%s: command not found\n", parts[0])
-		}
+func main() {
+	shell := NewShell()
+	if err := shell.Run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 }
