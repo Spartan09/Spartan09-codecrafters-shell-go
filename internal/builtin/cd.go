3d3e725025290eb8b1435914d39eb41ce559b39e
Ayushman <ayushsg0412@gmail.com>
2024-12-22 16:00:59 +0530

    codecrafters submit [skip ci]

--- internal/builtin/cd.go
+++ internal/builtin/cd.go
@@ -3,6 +3,7 @@ package builtin
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 type CdCommand struct{}
@@ -14,6 +15,15 @@ func (c CdCommand) Execute(args []string) error {
 	if len(args) > 1 {
 		return fmt.Errorf("cd: too many arguments")
 	}
+
+	if strings.HasPrefix(args[0], "~") {
+		homeDir, err := os.UserHomeDir()
+		if err != nil {
+			return err
+		}
+		args[0] = strings.Replace(args[0], "~", homeDir, 1)
+	}
+
 	err := os.Chdir(args[0])
 	if err != nil {
 		return fmt.Errorf("cd: %s: No such file or directory", args[0])
